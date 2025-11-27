3368043ff05e39e47251fd9b4791484f606541fd
NickMilin <nicholas.milin@gmail.com>
2025-11-27 17:59:09 -0500

    Created outline of LSTM model

---
+++ LSTM_model.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import time
+
+class LSTMModel(nn.Module):
+    def __init__(self):
+        super(LSTMModel, self).__init__()
+    
+    def forward(self, x):
+        pass
+
+    def predict(self, x):
+        pass
+
+    def evaluate_acc(self, true, pred):
+        pass
+    
+
+    
+    
